4374d8318cc5acb7f0a7e94f78402e9d3e636a7d
Bruno Neves <nevesbruno814@gmail.com>
2026-04-30 20:55:45 -0400

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"github.com/BrunoNeves1995/api-portunities/config"
 	"github.com/BrunoNeves1995/api-portunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Config
+	err := config.InitDB()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
 	//Initialize router
 	router.Initialize()
 
